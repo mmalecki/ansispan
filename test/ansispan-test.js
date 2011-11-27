e5d202cab2ca5bbadf55f251100a79680cd1c6c1
Maciej Małecki <maciej.malecki@notimplemented.org>
2011-11-27 14:24:02 +0100

    [test] Basic test suite

---
+++ test/ansispan-test.js
@@ -0,0 +1,47 @@
+var assert = require('assert'),
+    vows = require('vows'),
+    ansispan = require('../');
+
+require('colors');
+
+var dataSets = {
+  simple: {
+    input: 'hello world'.green,
+    output: '<span style="color: green">hello world</span>'
+  },
+  nested: {
+    input: 'hello world'.green.red,
+    output: '<span style="color: red"><span style="color: green">hello world</span></span>'
+  },
+  many: {
+    input: 'hello '.green + 'world'.red,
+    output: '<span style="color: green">hello </span><span style="color: red">world</span>'
+  },
+  'many same colors': {
+    input: 'hello '.green + 'world'.green,
+    output: '<span style="color: green">hello </span><span style="color: green">world</span>'
+  },
+  'colors with \\033\\[0;Xm': {
+    input: '\033\[0;32mhello world\033\[39m',
+    output: '<span style="color: green">hello world</span>'
+  }
+};
+
+function getTopics() {
+  var topics = {};
+  Object.keys(dataSets).forEach(function (set) {
+    topics['when using ' + set + ' data set'] = {
+      topic: ansispan(dataSets[set].input),
+      'should return correct output': function (result) {
+        assert.isString(result);
+        assert.equal(result, dataSets[set].output);
+      }
+    };
+  });
+  return topics;
+}
+
+vows.describe('ansispan').addBatch({
+  'When using ansispan': getTopics()
+}).export(module);
+
